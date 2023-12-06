7d18797e3f0be448e0b4cb2bc8e4e7e194034dfb
kader01agi <md.abdul.kader.ru@gmail.com>
2023-12-06 20:57:42 +0600

    context and loop

--- playProject/tempapp/views.py
+++ playProject/tempapp/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    # return render(request, 'home.html')
     return render(request, 'tempapp/home.html')
 
 def read(request):
-    return render(request, 'tempapp/read.html')
+    name = "MD. ABDUL KADER"
+    country = "Bangladesh"
+    skills = ["Python", "Django", "HTML"]
+    data = {"name": name, "country": country, "skills": skills}
+    return render(request, 'tempapp/read.html', data)
 
 def about(request):
     return render(request, 'tempapp/about.html')
